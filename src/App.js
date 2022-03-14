87ea24f94e8c057614933561aa1e2ee17db82cff
Rasujon3 <rasujon3@gmail.com>
2022-03-14 13:40:52 +0600

    add to favourite

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from "./components/MovieList.jsx";
 import MovieListHeading from "./components/MovieListHeading.jsx";
 import SearchBox from "./components/SearchBox.jsx";
+import AddFavourites from "./components/AddFavourites.jsx";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [favourites, setFavourites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=1340ef34`;
@@ -24,6 +26,11 @@ function App() {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  const AddFavouriteMovie = (movie) => {
+    const newFavoutieList = [...favourites, movie];
+    setFavourites(newFavoutieList);
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -31,7 +38,17 @@ function App() {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={AddFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList movies={favourites} />
       </div>
     </div>
   );
